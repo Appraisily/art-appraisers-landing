15d35c4ba047d82cba4bcdec5faf6d31a9dda290
Appraisily <134934621+Appraisily@users.noreply.github.com>
2025-02-25 11:05:23 +0100

    ssa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DevNav from './components/DevNav';
 
-import PaintingAppraisal from './pages/PaintingAppraisal';
 import ArtAppraiser from './pages/ArtAppraiser';
 
 const App = () => {
@@ -12,8 +11,7 @@ const App = () => {
     <BrowserRouter>
       <div className="min-h-screen bg-white">
         <Routes>
-          <Route path="/" element={<Navigate to="/painting-value" replace />} />
-          <Route path="/painting-value" element={<PaintingAppraisal />} />
+          <Route path="/" element={<ArtAppraiser />} />
           <Route path="/art-appraiser" element={<ArtAppraiser />} />
         </Routes>
         <DevNav />
